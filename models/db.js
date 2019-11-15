546ff64a652c937ade2ba4b8b179d88ed4143559
ronloh <wenheloh@hotmail.com>
2019-11-15 18:00:37 +0800

    change db connection

--- models/db.js
+++ models/db.js
@@ -2,11 +2,18 @@
 
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'locst',
+/* var connection = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: '',
     database: 'express'
+}) */
+
+var connection = mysql.createConnection({
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12312172',
+    password: 'aFz5xysVdB',
+    database: 'sql12312172'
 })
 
 module.exports = connection; 
